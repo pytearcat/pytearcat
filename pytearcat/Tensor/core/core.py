3ea19aaee5e32aa0e285f746ce58e16a36095a68
Joaquin <jmsureda@uc.cl>
2021-03-10 17:19:45 -0300

    Check for modules and define core

--- pytearcat/Tensor/core/core.py
+++ pytearcat/Tensor/core/core.py
@@ -1,25 +1,49 @@
 from IPython.display import display as display_IP, Math as Math_IP, Latex as Latex_IP
-from platform import system
+#from platform import system
+import pkg_resources
 
-OS = system()
+__required = {'jupyter','numpy' ,'sympy', 'tqdm','giacpy'}
+__installed = {pkg.key for pkg in pkg_resources.working_set}
+__missing = __required - __installed
 
-if OS == "Darwin": # macOS
-    
-    core_calc = "sp"
-    
-elif OS == "Windows" or OS == "Linux":
-    
-    core_calc = "gp"
-    
-else: # Not determined
+if len(__missing) != 0:
 
-    core_calc = "sp"
+    if 'giacpy' in _missing and len(__missing) == 1:
+
+        pass
+
+    else:
+
+        raise(EnvironmentError("There are missing modules: %s"(missing-{'giacpy'})))
+
+if 'giacpy' in __installed:
+
+    core_calc = 'gp'
+
+else:
+
+    core_calc = 'sp'
+
+# OS = system()
+
+# if OS == "Darwin": # macOS
+    
+#     core_calc = "sp"
+    
+# elif OS == "Windows" or OS == "Linux":
+    
+#     core_calc = "gp"
+    
+# else: # Not determined
+
+#     core_calc = "sp"
 
 if core_calc == 'gp':
 
     from giacpy import *
     import io
     from contextlib import redirect_stdout
+    from sympy import simplify as sp_simplify, sympify as sp_sympify, latex as sp_latex, Array as sp_Array
 
     def get_name(element):
         
@@ -49,18 +73,3 @@ elif core_calc == 'sp':
     def get_name(f):
 
         return f.name
-
-elif core_calc == 'se':
-
-    from symengine import *
-
-    def get_name(f):
-
-        return f.get_name()
-
-
-
-
-
-
-
