d3b5d959449b9a8e29ae76df4e7589f6eaba1ee1
Joaquin <jmsureda@uc.cl>
2021-05-10 21:39:08 -0400

    GR module renamed as gr

--- pytearcat/__init__.py
+++ pytearcat/__init__.py
@@ -1,4 +1,4 @@
-from .GR import def_coords, create_metric, christoffel, riemann, ricci, ricci_scalar, einstein, geodesic
+from .gr import def_coords, create_metric, christoffel, riemann, ricci, ricci_scalar, einstein, geodesic
 
 from .tensor import (setorder, series, new_var, new_con, new_fun, new_ten,
                     LeviCivita, KroneckerDelta, D,C,display, set_space_time,simplify)
