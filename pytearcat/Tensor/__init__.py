15e748014e386008345fbfc9899f7798a56e5a70
Joaquin <jmsureda@uc.cl>
2021-05-10 21:24:34 -0400

    LeviCivita.py renamed as lcivita.py

--- pytearcat/Tensor/__init__.py
+++ pytearcat/Tensor/__init__.py
@@ -1,5 +1,5 @@
 from .misc import setorder, series, new_var, new_con, new_fun, new_ten, set_space_time, simplify_pt as simplify
 from .tensor_class import D, C
 from .core import display
-from .LeviCivita import LeviCivita
+from .lcivita import LeviCivita
 from .kdelta import KroneckerDelta
