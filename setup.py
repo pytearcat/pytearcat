37166eeb766407d5a1b682b2efed6caff5e8221f
Joaquin <jmsureda@uc.cl>
2022-06-16 17:10:29 +0200

    Updated setup for version 0.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ ext_requirements = {"giacpy" : ["giacpy>=0.7.0"]}
 
 setup(
     name="pytearcat",
-    version="0.0.4",
+    version="0.0.5",
     author="Joaquin Sureda",
     author_email="jmsureda@uc.cl",
     description="A package to perform tensor algebra calculations.",
